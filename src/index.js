ad40fca4c257f4ed967aa217d4d258d207cd5f8b
Elias H <98331725+Suedie@users.noreply.github.com>
2024-05-01 00:26:44 +0200

    Change pipeline to initialise as new object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { pipeline } from '@xenova/transformers';
+import { TextGenerationPipeline, pipeline } from '@xenova/transformers';
 import { Client, IntentsBitField} from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-let pipe = await pipeline('text-generation', 'microsoft/phi-1_5');
+const pipe = new TextGenerationPipeline('microsoft/phi-1_5');
 
 const client = new Client({
     intents: [
@@ -17,10 +17,11 @@ const client = new Client({
 
 client.on('messageCreate', function (message) {
     if (message.author.bot) return;
-
-    const generatedText = pipe._call(`${message.content}`);
-
-    return message.reply(generatedText);
+    else if (message.mentions.has(client.user.id)) {
+        const text = message.content.replace(/<.*>/, '').trim();
+        const output = pipe(text);
+        return message.reply(text);
+    } else return;
   });
 
 client.login(process.env.BOT_TOKEN);
