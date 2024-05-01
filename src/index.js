ade3463d811fa9d75682d4d9454df973e4cb8ef7
Elias H <98331725+Suedie@users.noreply.github.com>
2024-05-01 04:23:17 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pipe = await pipeline('text-generation', 'Xenova/tamillama_tiny_30m');
+let pipe = await pipeline('text-generation', 'Xenova/phi-1_5_dev');
 
 const client = new Client({
     intents: [
@@ -15,36 +15,33 @@ const client = new Client({
     ],
 });
 
-
-let messages = [
-    {
-        role: "system",
-        content: "You are a direct and honest chatbot.",
-    },
-]
-
+//defines a chat template
 const chat = [
-    { role: "user", content: "Hello, how are you?" },
-    { role: "assistant", content: "I'm doing great. How can I help you today?" },
-    { role: "user", content: "I'd like to show off how chat templating works!" },
-]
+    { role: "System", content: "I am a honest chatbot." },
+  ]
 
 client.on('messageCreate', async function (message) {
+
     if (message.author.bot) return;
+
     else if (message.mentions.has(client.user.id)) {
 
         const text = message.content.replace(/<.*>/, '').trim();
 
-        messages.push({role: "user", content: text});
+        //Adds the message to the chat template
+        chat.push({role: "user", content: text});
 
-        const output = await pipe(messages, 128);
+        let output = await pipe(chat, 128);
 
         const index = output[output.length-1].generated_text.length-1;
 
         console.log(output[output.length-1].generated_text[index].content);
 
         return message.reply(output[output.length-1].generated_text[index].content);
-    } else return;
+    } 
+    
+    else return;
+
   });
 
 client.login(process.env.BOT_TOKEN);
