b9f9fde4a346945ee8c9e690da2f24dc97df4a9d
Elias H <98331725+Suedie@users.noreply.github.com>
2024-05-01 19:03:39 +0200

    Switch to manual from pipeline, clean up and add comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import { TextGenerationPipeline, pipeline } from '@xenova/transformers';
+import { AutoModel, AutoModelForCausalLM, AutoTokenizer, pipeline } from '@xenova/transformers';
 import { Client, IntentsBitField} from 'discord.js';
 import dotenv from 'dotenv';
 
-//Loads token from external file
+//used to load token from external file
 dotenv.config();
 
-let pipe = await pipeline('text-generation', 'Xenova/phi-1_5_dev');
+
+//link to language model from hugging face
+//https://huggingface.co/models?pipeline_tag=text-generation&library=transformers.js
+const source = 'Xenova/phi-1_5_dev';
+
+const tokenizer = await AutoTokenizer.from_pretrained(source);
+const model = await AutoModelForCausalLM.from_pretrained(source);
 
 const client = new Client({
     intents: [
@@ -16,10 +22,10 @@ const client = new Client({
     ],
 });
 
-//defines a chat template
+//List of chat messages, model should hook from here and add responses
 let chat = [
-    { role: "System", content: "I am a honest chatbot." },
-  ]
+    { role: "system", content: "You are an honest chatbot." },
+]
 
 client.on('messageCreate', async function (message) {
 
@@ -27,18 +33,23 @@ client.on('messageCreate', async function (message) {
 
     else if (message.mentions.has(client.user.id)) {
 
+        //Removes the username @ from the discord message
         const text = message.content.replace(/<.*>/, '').trim();
 
-        //Adds the message to the chat template
+        //Adds the message to the messages list
         chat.push({role: "user", content: text});
 
-        let output = await pipe(chat, 128);
+        //Uses default chat template
+        //Generation prompt adds a "assistant" JSON object to the list of messages and the model will generate the content as its response
+        const tokenized_chat = tokenizer.apply_chat_template(chat, {add_generation_prompt: true, tokenize: true});
 
-        const index = output[output.length-1].generated_text.length-1;
+        //await resolves the promise object returned from generate
+        //should generate a response but for some reason does nothing
+        let outputs = await model.generate(tokenized_chat);
 
-        console.log(output[output.length-1].generated_text[index].content);
+        let reply = tokenizer.decode(outputs[0], {skip_special_tokens: true});
 
-        return message.reply(output[output.length-1].generated_text[index].content);
+        return message.reply(reply);
     } 
     
     else return;
