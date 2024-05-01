2f83b2c9756d804014a57319a3447c7982dfd1a0
Elias H <98331725+Suedie@users.noreply.github.com>
2024-05-01 03:44:46 +0200

    Add chat template, non working

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pipe = await pipeline('text-generation', 'Xenova/phi-1_5_dev');
+const pipe = await pipeline('text-generation', 'Xenova/tamillama_tiny_30m');
 
 const client = new Client({
     intents: [
@@ -15,12 +15,35 @@ const client = new Client({
     ],
 });
 
+
+let messages = [
+    {
+        role: "system",
+        content: "You are a direct and honest chatbot.",
+    },
+]
+
+const chat = [
+    { role: "user", content: "Hello, how are you?" },
+    { role: "assistant", content: "I'm doing great. How can I help you today?" },
+    { role: "user", content: "I'd like to show off how chat templating works!" },
+]
+
 client.on('messageCreate', async function (message) {
     if (message.author.bot) return;
     else if (message.mentions.has(client.user.id)) {
+
         const text = message.content.replace(/<.*>/, '').trim();
-        const output = await pipe(text);
-        return message.reply(output[0].generated_text);
+
+        messages.push({role: "user", content: text});
+
+        const output = await pipe(messages, 128);
+
+        const index = output[output.length-1].generated_text.length-1;
+
+        console.log(output[output.length-1].generated_text[index].content);
+
+        return message.reply(output[output.length-1].generated_text[index].content);
     } else return;
   });
 
